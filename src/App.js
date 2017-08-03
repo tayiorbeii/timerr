da66e0607026032346e839b1230c3a77d7b99834
Taylor Bell <taylorbell@gmail.com>
2017-08-03 15:12:58 -0600

    basic countdown

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
 import { observable } from 'mobx'
 import { observer } from 'mobx-react'
-import logo from './logo.svg';
+import ReactInterval from 'react-interval'
+import convert from 'convert-seconds'
+import Devtools from 'mobx-react-devtools'
+
 
 const minToSec = (min) => min * 60
 
 const timerState = observable({
-  timer: 10
+  workPeriod: minToSec(1),
+  timer: 0,
+  pausePeriod: 0,
+  breakCount: 0,
+  enabled: false
 })
 
 const App = observer(class App extends Component {
-  constructor (props) {
-    super(props)
-        
+  componentWillMount () {
+    timerState.timer = timerState.workPeriod
   }
 
-  startTimer = () => {
-    
+  toggleEnabled = () => {
+    timerState.enabled = !timerState.enabled 
   }
 
   render () {
-  
+    
+    const { timer, breakCount, workPeriod, pausePeriod, enabled } = timerState
+
     return (
-      <div className='ma4'>
-        {timerState.timer}
+      <div>
+        <ReactInterval {...{timer, enabled}}
+          callback={() => timerState.timer -= 1}
+        />
+
+        <div className='ma4'>
+          {`${convert(timer).minutes}:${convert(timer).seconds.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})}`}
+        </div>
+      
+        <div className=''>
+          <button
+            onClick={() => this.toggleEnabled()}
+          >
+            {!enabled && breakCount === 0 ? 'Start' : 'Pause'}
+          </button>
+        </div>
       </div>
     )
   }
